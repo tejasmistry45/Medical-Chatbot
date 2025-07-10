db12813ac9add1470fb0a15ed8b649d8037b88b1
mistrytejasm <mistrytejasm45@gmail.com>
2025-07-10 16:49:55 +0530

    memory and reset option is added

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,16 @@
 from typing import TypedDict, Annotated, List
 from langgraph.graph import add_messages, StateGraph, END
 from langchain_groq import ChatGroq
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessage
 from dotenv import load_dotenv
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 
 load_dotenv()
+
+# database connection
+sqlite_conn = sqlite3.connect("DataBase/checkpoint.sqlite", check_same_thread=False)
+memory = SqliteSaver(sqlite_conn)
 
 # llm
 llm = ChatGroq(model="llama3-70b-8192")
@@ -16,13 +22,22 @@ class AgentState(TypedDict):
 # create a node
 def chatbot(state: AgentState):
 
-    messages = state['messages']
-    response = llm.invoke(messages)
-    updated_messages = messages + [response]
+    # get full message history
+    messages = state["messages"]
+
+    # Keep only the last 10 messages (5 Human + 5 AI)
+    recent_messages = messages[-10:]
+
+    # Generate response from LLM using recent context
+    response = llm.invoke(recent_messages)
+
+    # add new AI message to the messages
+    updated_messages = recent_messages + [response]
 
     return {
         "messages": updated_messages
     }
+
 
 graph = StateGraph(AgentState)
 
@@ -30,21 +45,27 @@ graph.set_entry_point("chatbot_node")
 graph.add_node("chatbot_node", chatbot)
 graph.add_edge("chatbot_node", END)
 
-app = graph.compile()
+app = graph.compile(checkpointer=memory)
 
-config = {"configurable": {"thread_id":1}}
+config = {"configurable": {"thread_id": "1" }}
 
 if __name__ == "__main__":
     print("Simple ChatBot is Ready")
     print("=" * 80)
 
     while True:
+        
         user_query = input("User: ")
         print("=" * 80)
         if user_query == "exit":
-            print("GoodBay See You soon :)")
+            print('GoodBay See You soon :)')
             break
-        
+
+        if user_query.lower() == 'reset':
+            memory.delete_thread("1")  # Match the thread_id string
+            print("Memory Has been Reset")
+            continue
+
         result = app.invoke({
             'messages': [HumanMessage(content=user_query)],
         }, config = config)
